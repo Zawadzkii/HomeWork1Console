c7cf83163827cb4c85e9ac90afdc6e2c18657270
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-03-21 17:54:39 +0100

    Add project files.

---
+++ HomeWorkConsole1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		// Declaration of variables related to employee data
+		string firstName = "Harry";
+		string lastName = "Keayn";
+		int age = 25;
+		char gender = 'm'; 
+		string pesel = "12345678901";
+		long employeeNumber = 2509324094;
+
+		// Displaying employee data
+		Console.WriteLine("Employee Data:");
+		Console.WriteLine("First Name: " + firstName);
+		Console.WriteLine("Last Name: " + lastName);
+		Console.WriteLine("Age: " + age);
+		Console.WriteLine("Gender: " + gender);
+		Console.WriteLine("PESEL: " + pesel);
+		Console.WriteLine("Employee Number: " + employeeNumber);
+
+		Console.ReadKey();
+	}
+}
